5ab3f9853c8149d977f325f7fe620fd1f395fdc5
kzkzkzkz29 <a.rocha03@ufromail.cl>
2025-07-10 00:38:41 -0400

    Agregada la clase FirebaseConfig para inicializar FirebaseApp con las credenciales del bucket

--- backend/src/main/java/com/echowave/backend/config/FirebaseConfig.java
+++ backend/src/main/java/com/echowave/backend/config/FirebaseConfig.java
@@ -1,2 +1,30 @@
-package com.echowave.backend.config;public class FirebaseConfig {
+package com.echowave.backend.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void init() throws IOException {
+        InputStream serviceAccount =
+                getClass().getClassLoader().getResourceAsStream("firebase/echowave-b2c81-firebase-adminsdk-fbsvc-74acbb80a9.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setStorageBucket("echowave-b2c81")
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options);
+        }
+    }
 }
+
