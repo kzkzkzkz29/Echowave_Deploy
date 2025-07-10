69414b3f43e586300d400484cfa91fb79e7afa17
kzkzkzkz29 <a.rocha03@ufromail.cl>
2025-07-10 00:55:13 -0400

    Creando AudioRepositoty

--- backend/src/main/java/com/echowave/backend/repository/AudioRepository.java
+++ backend/src/main/java/com/echowave/backend/repository/AudioRepository.java
@@ -1,2 +1,10 @@
-package com.echowave.backend.repository;public interface AudioRepository {
+package com.echowave.backend.repository;
+
+import com.echowave.backend.entity.AudioEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AudioRepository extends JpaRepository<AudioEntity, Long> {
+    List<AudioEntity> findBySessionId(String sessionId);
 }
