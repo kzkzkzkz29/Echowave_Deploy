09c94d718e8a9a3a60384128d9ef650e7fde38ad
kzkzkzkz29 <a.rocha03@ufromail.cl>
2025-07-10 00:40:26 -0400

    Agregada interfaz ArchivoUploader

--- backend/src/main/java/com/echowave/backend/service/ArchivoUploader.java
+++ backend/src/main/java/com/echowave/backend/service/ArchivoUploader.java
@@ -1,2 +1,10 @@
-package com.echowave.backend.service;public class ArchivoUploader {
+package com.echowave.backend.service;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface ArchivoUploader {
+    String subirArchivo(MultipartFile archivo, HttpSession session) throws IOException;
 }
