e8ee4944fe3441143fe859e93453986ffaf69225
kzkzkzkz29 <a.rocha03@ufromail.cl>
2025-07-10 00:45:09 -0400

    Agregando clase para subir archivos a Firebase Storage

--- backend/src/main/java/com/echowave/backend/service/FirebaseUploaderService.java
+++ backend/src/main/java/com/echowave/backend/service/FirebaseUploaderService.java
@@ -1,2 +1,41 @@
-package com.echowave.backend.service;public class FirebaseUploaderService {
+package com.echowave.backend.service;
+
+import com.google.cloud.storage.*;
+import com.google.firebase.cloud.StorageClient;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+public class FirebaseUploaderService implements ArchivoUploader {
+
+    @Override
+    public String subirArchivo(MultipartFile archivo, HttpSession session) throws IOException {
+        String nombreArchivo = "audios/" + UUID.randomUUID() + "-" + archivo.getOriginalFilename();
+
+        String token = UUID.randomUUID().toString();
+
+        Storage storage = StorageClient.getInstance().bucket().getStorage();
+
+        BlobId blobId = BlobId.of("echowave-b2c81", nombreArchivo);
+        BlobInfo blobInfo = BlobInfo.newBuilder(blobId)
+                .setContentType(archivo.getContentType())
+                .setMetadata(Map.of("firebaseStorageDownloadTokens", token))
+                .build();
+
+        storage.create(blobInfo, archivo.getBytes());
+
+        return String.format(
+                "https://firebasestorage.googleapis.com/v0/b/%s/o/%s?alt=media&token=%s",
+                "echowave-b2c81",
+                URLEncoder.encode(nombreArchivo, StandardCharsets.UTF_8),
+                token
+        );
+    }
 }
